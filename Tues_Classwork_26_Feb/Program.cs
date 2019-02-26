cc441aa23c34f254eb0332346abc6a90c5acada5
Samuel Lane <samuel.lane.sa@outlook.com>
2019-02-26 09:43:10 -0500

    Classwork Tues 26 Feb 2019

--- Tues_Classwork_26_Feb/Program.cs
+++ Tues_Classwork_26_Feb/Program.cs
@@ -10,6 +10,15 @@ namespace Tues_Classwork_26_Feb
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(M(i: 15));
+            Console.WriteLine(M(15, 3));
+            Console.WriteLine(M(i: 15, offset: 3));
+            Console.WriteLine(M(scale: 7, i: 3));
+            Console.ReadLine();
         }
+
+        static int M(int i) => M(i, 1);
+        static int M(int i, int scale) => M(i, scale, 0);
+        static int M(int i, int scale = 1, int offset = 0) => offset + i * scale;
     }
 }
